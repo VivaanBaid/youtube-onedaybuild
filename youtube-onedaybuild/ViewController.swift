b36f74aeb386beccfb80b103543324b5d44ca661
Vivaan Baid <vivaanbaid@Vivaans-MacBook-Air-6.local>
2021-07-12 23:53:39 +0530

    initial xcode project

---
+++ youtube-onedaybuild/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by Vivaan Baid on 12/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
