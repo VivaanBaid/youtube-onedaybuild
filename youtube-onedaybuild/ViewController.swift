4ae1c6bff56f38b3a4c72c55f12fec961c80bc91
Vivaan Baid <vivaanbaid@Vivaans-MacBook-Air-6.local>
2021-07-13 15:05:12 +0530

    Networking call update

--- youtube-onedaybuild/ViewController.swift
+++ youtube-onedaybuild/ViewController.swift
@@ -8,10 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var Manager = manager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        Manager.getvideos()
     }
 
 
